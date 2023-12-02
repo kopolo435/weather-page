3461b01df5c65e94cb787743c2ef737e06c3cc94
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-02 12:46:34 -0500

    Modifica nombre de funcion de fetchWeather

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,10 +1,10 @@
-async function getForecast(location) {
+async function getWeatherData(location) {
   const key = "1135fa4c264d495a985173013232611";
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json ?key=${key}&q=${location}&days=2&aqi=yes`,
     { mode: "cors" }
   );
-  // return response.json().then((data) => data);
+
   const data = await response.json();
   return data;
 }
