1e3e109afdf711b53c99b0bb63879019665c24e9
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-07 09:20:30 -0500

    Crea funcion que obtiene el weather y forecast deseado
    Esta funcion tiene como parametro la localidad desad, luego llama
    a la funcion de fetchwether la cual retorna una promise y al cumplirse
    se crean los datos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import getForecast from "./fetchWeather";
+import getWeatherData from "./fetchWeather";
+import * as weather from "./dataHandler";
 
-getForecast("Colón,Panamá");
+function searchForecast(location) {
+  getWeatherData(location).then((data) => {
+    console.log(data);
+
+    const currentWeather = weather.handleCurrentWeather(data);
+    const forecastWeather = weather.getForecast(
+      data.forecast.forecastday,
+      data.location.localtime
+    );
+    console.log(currentWeather);
+    console.log(forecastWeather);
+  });
+}
+
+searchForecast("London");
