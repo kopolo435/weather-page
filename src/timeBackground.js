c19e90b04c1b73e6c9a90e9ce297da16e305beff
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-10 14:06:59 -0500

    Agrega funcion para modifica color de aside Icons
    Estos colores cambian dependiendo de la hora de la localidad buscada

--- src/timeBackground.js
+++ src/timeBackground.js
@@ -1,13 +1,31 @@
+function setDayIcons() {
+  const icons = document.getElementsByClassName("asideIcon");
+  Array.from(icons).forEach((icon) => {
+    const path = icon.querySelector("path");
+    path.setAttribute("stroke", "#000000");
+  });
+}
+
+function setNightIcons() {
+  const icons = document.getElementsByClassName("asideIcon");
+  Array.from(icons).forEach((icon) => {
+    const path = icon.querySelector("path");
+    path.setAttribute("stroke", "#ffffff");
+  });
+}
+
 function setDayBackground() {
   const { body } = document;
   body.classList.remove("night");
   body.classList.add("sunny");
+  setDayIcons();
 }
 
 function setNightBackground() {
   const { body } = document;
   body.classList.remove("sunny");
   body.classList.add("night");
+  setNightIcons();
 }
 
 function setTimeBackground(isDay) {
