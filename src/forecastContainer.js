f838e55cbc372d74e0100a37cafc6a6e51f08a97
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-11 14:52:30 -0500

    Agrega funcion para agregar img a card de forecast
    Tambien añade el export de la funcion principal de creacion

--- src/forecastContainer.js
+++ src/forecastContainer.js
@@ -5,10 +5,19 @@ function createTextLine(text) {
   return textElement;
 }
 
+function createImg(url) {
+  const img = document.createElement("img");
+  img.src = url;
+  return img;
+}
+
 function createForecastContainers(forecast, unitSystem) {
   const forecastContainer = document.getElementById("forecastContainer");
   forecast.forEach((day) => {
     forecastContainer.appendChild(createTextLine(day.date));
-    forecastContainer.appendChild(createTextLine(day.date));
+    forecastContainer.appendChild(createImg(day.iconCondition));
+    forecastContainer.appendChild(createTextLine(day.dayCondition));
   });
 }
+
+export default createForecastContainers;
