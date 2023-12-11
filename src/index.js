052ae3c54db6d97c9f9d666d0a4cf15d65504f15
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-12-11 17:52:17 -0500

    Agrega funcionalidad de evento para mostrar sidebar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import * as dataHandler from "./dataHandler";
 import updateLocation from "./updateLocation";
 import setTimeBackground from "./timeBackground";
 import createForecastContainers from "./forecastContainer";
+import displayHourForecast from "./displayHourForecast";
 import style from "./style.css";
 
 const searchForm = document.getElementById("searchForm");
 const searchBar = document.getElementById("search");
 const selectUnitSystem = document.getElementById("unidad");
+const sidebar = document.getElementById("hourForecast");
+const closeBtn = document.getElementById("return");
 let unitSystem = "metrico";
 let currentWeather;
 let forecastWeather;
@@ -23,11 +26,28 @@ async function searchForecast(location) {
   return { currentWeather, forecastWeather };
 }
 
+function showHourForecast(event) {
+  const { id } = event.target.closest("[data-id]").dataset;
+  displayHourForecast(forecastWeather[id]);
+}
+
+function addDayCardEvent() {
+  const cards = document.getElementsByClassName("dayCard");
+  Array.from(cards).forEach((day) => {
+    day.addEventListener("click", showHourForecast);
+  });
+}
+
 function updateDisplay(weather) {
   console.log(weather);
   updateLocation(weather.currentWeather, weather.forecastWeather, unitSystem);
   createForecastContainers(forecastWeather, unitSystem);
   setTimeBackground(weather.currentWeather.isDay);
+  addDayCardEvent();
+}
+
+function changeSideBarVisibility() {
+  sidebar.classList.add("visible");
 }
 
 searchForm.addEventListener("submit", (e) => {
@@ -38,6 +58,11 @@ searchForm.addEventListener("submit", (e) => {
 selectUnitSystem.addEventListener("change", () => {
   unitSystem = unitSystem === "imperial" ? "metrico" : "imperial";
   updateDisplay({ currentWeather, forecastWeather });
+  changeSideBarVisibility();
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("visible");
 });
 
 searchForecast("Panama,Panamá").then(updateDisplay);
